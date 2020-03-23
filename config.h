a9fa45bf690de2570be0bc6b25d55a9758d7e3b6
cy <root@rescld.cn>
2020-03-23 12:57:02 +0800

    Add patche copyurl

--- config.h
+++ config.h
@@ -187,6 +187,8 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 
+	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
+
 	/* Scroll */
 	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
 	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
